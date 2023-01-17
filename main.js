c8c4ccbcee28a903ea5fff44e0cbfe933bd29a4b
Aborjs <122416179+Aborjs@users.noreply.github.com>
2023-01-17 08:44:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,16 @@ while (true) {
     if(n == null) {
         main.style.display = "none";
         cancel.style.display = "flex";
-        msg.innerHTML = "Thank you!!!";
+        msg.innerHTML = "what!!! kung ako 'yan hindi kita ginaganyan because as an atenean you deserve guys like me uh uh omsim";
     }
 
     if (n.length > 0) {
         alert("hi ako parin ba");
         alert('pano pag hindi na');
         alert('pano nako');
-        alert('hindi ko na alam gagawin kung wala ka');
+        alert('bakit');
+        alert('ka nagiba');
+        alert('meron nabang iba');
         alert('i like ya cut g');
         alert('I love You ' + n);
         main.style.display = 'block';
